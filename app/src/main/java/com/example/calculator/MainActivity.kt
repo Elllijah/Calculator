4a967966a3771272a66167a16526fd7d7d3cdfa8
Elllijah <ilya.esaulov.nsk@gmail.com>
2021-11-08 17:58:45 +0300

    Del, AC, Calucation, History, Clear History

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.calculator
 
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.objecthunter.exp4j.ExpressionBuilder
@@ -39,16 +43,62 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        fun addTextView(linLay : LinearLayout, viewId : Int, text : String, flag : String = "") {
+            val viewshechka = TextView(this)
+            viewshechka.text = text
+            viewshechka.id = viewId
+            var  param: LinearLayout.LayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,70)
+            viewshechka.layoutParams = param
+            viewshechka.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22F)
+            linLay.addView(viewshechka)
+            viewshechka.setOnClickListener{
+                if (flag.equals("=")){
+                    math_expression.text = viewshechka.text.substring(1, viewshechka.text.length)
+                } else {
+                    math_expression.text = viewshechka.text
+                }
+            }
+        }
+
+        var numVeiwId: Int = 1000
+        var mutListId = mutableListOf<Int>()
+        var mutListText = mutableListOf<String>()
+        var mutListRes = mutableListOf<String>()
+
         equal_btn.setOnClickListener{
             try{
                 val temp_exp: String = math_expression.text.toString()
                 val eb = ExpressionBuilder(math_expression.text.toString()).build()
                 val res = eb.evaluate()
-                math_expression.text = res.toString()
-                history.text = temp_exp
+
+                val longRes = res.toLong()
+                if (res == longRes.toDouble()){
+                    mutListRes.add(longRes.toString())
+                    math_expression.text = longRes.toString()
+                } else {
+                    mutListRes.add(res.toString())
+                    math_expression.text = res.toString()
+                }
+
+                mutListId.add(numVeiwId)
+                mutListText.add(temp_exp)
+//                mutListRes.add(res.toString())
+                addTextView(container, mutListId[numVeiwId - 1000],mutListText[numVeiwId - 1000])
+                addTextView(container, mutListId[numVeiwId - 1000] - 500, "=${mutListRes[numVeiwId - 1000]}", "=")
+
+                numVeiwId ++
+                scroll.post(Runnable { scroll.fullScroll(ScrollView.FOCUS_DOWN)  })
 
             } catch(e:Exception) {
                 Log.d("Ошибка:","${e.message}")
+            }
+
+            button.setOnClickListener{
+                container.removeAllViews()
+                numVeiwId = 1000
+                mutListId = mutableListOf<Int>()
+                mutListText = mutableListOf<String>()
+                mutListRes = mutableListOf<String>()
             }
         }
     }
